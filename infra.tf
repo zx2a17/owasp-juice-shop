6a4f3e7b21dc34768955eedea86077c8cda1c946
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-07-25 11:36:00 +0800

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -57,6 +57,7 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
