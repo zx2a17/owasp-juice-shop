4f6c3c6ddf84eabef73f4b608abc9d0454df8433
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-07-25 12:58:53 +0800

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -15,7 +15,7 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
-  access_key  = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+  access_key  = "{ secrets.AWS_ACCESS_KEY_ID }"
   secret_key  = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
 }
 
